58dbe8f68ad872c07ad94f4fd08b13210c811a3c
Ubuntu <ubuntu@ip-172-31-18-157.ec2.internal>
2021-11-12 22:46:35 -0600

    modifications

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 # our database file path
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    'DATABASE_URL', 'sqlite:///data.db')
+    'DATABASE_URL2', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
