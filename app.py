ce1d19830ab8462240f1a161f09dfa608990d138
Ubuntu <ubuntu@ip-172-31-18-157.ec2.internal>
2021-11-12 20:27:02 -0600

    making changes to app.py

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     db.init_app(app)
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(port=8080, debug=True)
